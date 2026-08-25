24fab058e67c725ea21bdbb511aa386b5797aeb1
bowoxdaily <ikhwanudinaldi04@gmail.com>
2026-08-25 11:52:07 +0700

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -60,6 +60,7 @@
             rel="stylesheet">
     </noscript>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script>
         tailwind.config = {
